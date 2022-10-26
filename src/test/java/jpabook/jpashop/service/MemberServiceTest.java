32ad54bce756532d89c00d60b518660a45b7d084
portuna85 <portuna85@gmail.com>
2022-10-26 12:59:54 +0900

    Jpa Spring application

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -48,6 +47,7 @@ public class MemberServiceTest {
         // given
         Member member1 = new Member();
         member1.setName("kim");
+
         Member member2 = new Member();
         member2.setName("kim");
 
@@ -57,6 +57,5 @@ public class MemberServiceTest {
 
         // then
         fail("예외가 발생해야 한다.");
-
     }
 }
